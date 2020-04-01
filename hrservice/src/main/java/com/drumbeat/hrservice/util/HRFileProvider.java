94f7a1050612c362348b3e481dc59fa69a7940c8
wangyu <282729478@qq.com>
2020-04-01 10:22:28 +0800

    添加provider

---
+++ hrservice/src/main/java/com/drumbeat/hrservice/util/HRFileProvider.java
@@ -0,0 +1,11 @@
+package com.drumbeat.hrservice.util;
+
+import androidx.core.content.FileProvider;
+
+/**
+ * EssFileProvider
+ * Created by Atom on 2018/2/23.
+ */
+
+public class HRFileProvider extends FileProvider {
+}
